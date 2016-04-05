d21ed9ab591415b4cd7f2eb265cae2eb17cfb3e5
kapil33 <kapilphalswal3@gmail.com>
2016-04-05 12:48:15 +0530

    epay-models-created

--- epay/models.py
+++ epay/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from bus.models import Ticket
 from taxi.models import Booking
 from runner.models import User
+from django.utils.encoding import python_2_unicode_compatible
 STATUS_CHOICES=(
 	("DONE","DONE"),
 	("LATER","LATER"))
@@ -9,7 +10,8 @@ STATUS_CHOICES=(
 from django.db import models
 
 # Create your models here.
-class Wallet(model.Model):
+@python_2_unicode_compatible
+class Payment(model.Model):
 	id=models.AutoField(primary_key=True)
 	user=models.ForeignKey(User , models.DO_NOTHING , related_name="+",null=True)
 	ticket=models.ForeignKey(Ticket , models.DO_NOTHING , related_name="+",null=True)
@@ -18,3 +20,18 @@ class Wallet(model.Model):
 	amount=models.IntegerField(default=0)
 	token=models.CharField(max_length=200,default="")
 	status=models.CharField(max_length=20,default="",choices=STATUS_CHOICES)
+	def __str__(self):
+		return self.user.name+" "+str(transaction_date)
+
+@python_2_unicode_compatible
+class Wallet(models.Model):
+	id=models.AutoField(primary_key=True)
+	user=models.ForeignKey(User , models.DO_NOTHING , related_name="+",null=True)
+	transaction_date=models.DateTimeField()
+	amount=models.IntegerField(default=0)
+	token=models.CharField(max_length=200,default="")
+	def __str__(self):
+		return self.user.name+" "+str(transaction_date)
+
+	
+
