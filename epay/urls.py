ffa45f586435d14e71940ba313ca7706b3a22a6e
kapil33 <kapilphalswal3@gmail.com>
2016-04-05 17:12:02 +0530

    changes done to epay/views.py

--- epay/urls.py
+++ epay/urls.py
@@ -1,1 +1,8 @@
-"" 
+from django.conf.urls import url
+from epay import views
+urlpatterns = [
+	url(r'^pay-now/',views.pay_now ,name="pay_now"),
+	url(r'^pay-later/',views.pay_later ,name="pay_later"),
+	url(r'^add-wallet/',views.add_allet ,name="add-wallet"),
+]
+
--- epay/views.py
+++ epay/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+import MySQLdb as mysql
+from django.db import connection
+from epay.queries import *
+import json
+from epay.serializers import *
+from epay.serializers import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from epay import forms
+from rest_framework import viewsets
+from MySQLdb.cursors import SSDictCursor
+from django.core.mail import send_mail
+from epay.models import *
+import urllib , urllib2 
+from django.utils import timezone
 # Create your views here.
+@api_view(['POST'])
+def pay_now(request):
+	utoken=request.POST["user"]
+	amount=request.POST["amount"]
+	url="http://roadrunner.com/e-auth"
+	data={'token':utoken}
+	req=urllib2.Request(url , data)
+	res=urllib2.urlopen( req)
+	return HttpResponse(res)
+	
+
+
