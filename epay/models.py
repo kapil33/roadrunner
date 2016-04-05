60009d2ae09f737f6bdbebc8bb79302aef1acd22
kapil33 <kapilphalswal3@gmail.com>
2016-04-05 12:37:05 +0530

    epay-wallet-model-added

--- epay/models.py
+++ epay/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
+from bus.models import Ticket
+from taxi.models import Booking
+from runner.models import User
+STATUS_CHOICES=(
+	("DONE","DONE"),
+	("LATER","LATER"))
 
 from django.db import models
 
 # Create your models here.
+class Wallet(model.Model):
+	id=models.AutoField(primary_key=True)
+	user=models.ForeignKey(User , models.DO_NOTHING , related_name="+",null=True)
+	ticket=models.ForeignKey(Ticket , models.DO_NOTHING , related_name="+",null=True)
+	booking=models.ForeignKey(Booking , models.DO_NOTHING , related_name="+",null=True)
+	transaction_date=models.DateTimeField()
+	amount=models.IntegerField(default=0)
+	token=models.CharField(max_length=200,default="")
+	status=models.CharField(max_length=20,default="",choices=STATUS_CHOICES)
